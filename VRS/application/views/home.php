3601c8c1dcfbb6ad4c58f3b63671939c4d20cca3
RavenThevar <53144457+RavenThevar@users.noreply.github.com>
2019-09-25 23:13:49 +0800

    Optimisation done to 302CEM\VRS\application\views\home
    A database query is added.

--- VRS/application/views/home.php
+++ VRS/application/views/home.php
@@ -1,1 +1,16 @@
+</br>
+</br>
+</br>
+</br>
 <h1> Hello </h1>
+
+<?php
+		$data2 = array(
+			'venue_name' => 'Local',
+			'venue_capacity' => '22',
+			'venue_location' => 'Groove Street'
+            );
+
+            $this->db->insert('venue', $data2);
+
+?>
