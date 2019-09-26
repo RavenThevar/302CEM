1fd637b13d7e48a54e4e9b8fd51c29f08b59881b
RavenThevar <53144457+RavenThevar@users.noreply.github.com>
2019-09-27 07:56:42 +0800

    Optimisation done to 302CEM\VRS\application\views\home
    The delete of PHP queries upon deeming it as unnecessary.

--- VRS/application/views/home.php
+++ VRS/application/views/home.php
@@ -3,14 +3,3 @@
 </br>
 </br>
 <h1> Hello </h1>
-
-<?php
-		$data2 = array(
-			'venue_name' => 'Local',
-			'venue_capacity' => '22',
-			'venue_location' => 'Groove Street'
-            );
-
-            $this->db->insert('venue', $data2);
-
-?>
