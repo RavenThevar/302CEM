0db67a11cd112cd43d6e64d26295022a21a06d38
RavenThevar <53144457+RavenThevar@users.noreply.github.com>
2019-09-27 07:57:16 +0800

    Optimisation done to 302CEM\VRS\application\controller\Homepage
    A minor new line is added into this file.

--- VRS/application/controllers/Homepage.php
+++ VRS/application/controllers/Homepage.php
@@ -6,6 +6,7 @@ class Homepage extends CI_Controller
     public function index()
 	{
         $data['title'] = "VRS - HOME";
+        
         $this->load->database();
         $this->load->view('header', $data);
 		$this->load->view('home');
