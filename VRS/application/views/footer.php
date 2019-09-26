33f3aed4b179e404c6adbf33d17487eb0feb85ce
RavenThevar <53144457+RavenThevar@users.noreply.github.com>
2019-09-27 07:59:54 +0800

    Optimisation done to 302CEM\VRS\application\views\footer
    The arrangement of footer tagline is further organised to look better on the display.

--- VRS/application/views/footer.php
+++ VRS/application/views/footer.php
@@ -1,3 +1,6 @@
-        <h6> VRS - Venue Reservation System - 2019 - All rights reserved </h6>
+        </br>
+        <div class="container">
+            <h6> VRS - Venue Reservation System - 2019 - All rights reserved </h6>
+        </div>
     </body>
 </html>
