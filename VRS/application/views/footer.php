d27a2c0eca53a28c8d3bf0c6cfc493dff7e988f6
RavenThevar <53144457+RavenThevar@users.noreply.github.com>
2019-09-30 22:58:22 +0800

    Optimisation done to 302CEM\VRS\application\views\footer
    An additional system message is added to the page file.

--- VRS/application/views/footer.php
+++ VRS/application/views/footer.php
@@ -1,6 +1,7 @@
         </br>
         <div class="container">
             <h6> VRS - Venue Reservation System - 2019 - All rights reserved </h6>
+            <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds.</p>
         </div>
     </body>
 </html>
