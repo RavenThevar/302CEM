e32a0ddad2fc93a24eede2c7703b988f5bb1f1ad
RavenThevar <53144457+RavenThevar@users.noreply.github.com>
2019-10-04 18:46:58 +0800

    Optimisation done to 302CEM\VRS\application\views\venue_cust
    A new file is added under the folder "views". This file is added to support the venue booking by the customer. Data validation, database integration is added extensively.

---
+++ VRS/application/views/venue_cust.php
@@ -0,0 +1,91 @@
+</br>
+</br>
+</br>
+
+<div class ="container">
+    <div row>
+        <h1>Available Venues:</h1>
+    </div>
+    <table class="table table-hover table-striped table-bordered">
+        
+        <thead class="thead-light">
+            <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Capacity</th>
+                <th>Location</th>
+                <th>Availability</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <?php 
+                $query = $this->db->get('venue');
+
+                foreach ($query->result() as $row)
+                {
+                    if ($row->venue_avail == 1)
+                    {
+                        echo "<tr>";
+                        echo"<td><font color='black'>$row->venue_id</font></td>";
+                        echo"<td><font color='black'>$row->venue_name</font></td>";
+                        echo"<td><font color='black'>$row->venue_capacity</font></td>";
+                        echo"<td><font color='black'>$row->venue_location</font></td>";
+                        echo"<td><font color='black'>AVAILABLE</font></td>";
+                    }
+                }   
+                    echo "</tr>";
+            ?>
+        </tbody>
+
+    </table>    
+
+</div>
+
+</br>
+</br>
+
+<div class="container">
+    <div class="row">
+        <h1>Book Venue:</h1>
+    </div>
+
+    <form name = "venueCustChange" method = "POST">
+        <div class="col-lg-2">
+            <div class="form-group">
+                <label for="vID">#Venue ID:</label>
+                <input type="text" class="form-control" id="vID" placeholder="e.g 22" name="vID">
+            </div>
+        </div>
+        </br>
+        <div class="col-lg-2">
+            <button type="submit" class="btn btn-primary">BOOK</button>
+        </div>
+
+    </form>
+
+    <?php
+        if(isset ($_POST['vID']))
+        {
+            $data4 = array
+                (
+                'venue_avail' => 0
+                );
+            $this->db->set($data4);
+            $this->db->where('venue_id', $_POST['vID']);
+            $this->db->update('venue');
+
+            print "</br>
+            <div class = 'container'>
+             <font color = 'green'> You have successfully booked this venue. </font>
+            </div>
+            </br>
+           ";
+
+            $page = $_SERVER['PHP_SELF'];
+            $sec = "2";
+            header("Refresh: $sec; url=$page");
+        }
+    ?>
+
+</div>
