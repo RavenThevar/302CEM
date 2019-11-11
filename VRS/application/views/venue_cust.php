68ccb3fd7f4182a45ed0eec576b3811691f80b60
RavenThevar <53144457+RavenThevar@users.noreply.github.com>
2019-11-11 13:09:16 +0800

    Optimisation done to 302CEM\VRS\application\views\venue_cust
    A new feature is added to the program. This feature would allow the customers to view their booked venues. Further optimisation is done to make the venue availability status more appealing in the Customer page.

--- VRS/application/views/venue_cust.php
+++ VRS/application/views/venue_cust.php
@@ -31,7 +31,7 @@
                         echo"<td><font color='black'>$row->venue_name</font></td>";
                         echo"<td><font color='black'>$row->venue_capacity</font></td>";
                         echo"<td><font color='black'>$row->venue_location</font></td>";
-                        echo"<td><font color='black'>AVAILABLE</font></td>";
+                        echo"<td><font color='green'><b>AVAILABLE</b></font></td>";
                     }
                 }   
                     echo "</tr>";
@@ -54,7 +54,12 @@
         <div class="col-lg-2">
             <div class="form-group">
                 <label for="vID">#Venue ID:</label>
-                <input type="text" class="form-control" id="vID" placeholder="e.g 22" name="vID">
+                <input type="text" class="form-control" id="vID" placeholder="e.g: 22" name="vID">
+            </div>
+
+            <div class="form-group">
+                <label for="vE">#Book Until:</label>
+                <input type="text" class="form-control" id="vE" placeholder="e.g: 12/12/2019" name="vE">
             </div>
         </div>
         </br>
@@ -69,7 +74,8 @@
         {
             $data1 = array
                 (
-                'venue_avail' => 0
+                'venue_avail' => 0,
+                'venue_expiry' => $_POST['vE']
                 );
             $this->db->set($data1);
             $this->db->where('venue_id', $_POST['vID']);
@@ -94,5 +100,56 @@
             header("Refresh: $sec; url=$page");
         }
     ?>
+
+</div>
+
+</br>
+</br>
+
+<div class ="container">
+    <div row>
+        <h1>Your Booked Venues:</h1>
+    </div>
+    <table class="table table-hover table-striped table-bordered">
+        
+        <thead class="thead-light">
+            <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Capacity</th>
+                <th>Location</th>
+                <th>Due On</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <?php 
+                $query = $this->db->get('booking');
+                $query2 = $this->db->get('venue');
+
+                foreach ($query->result() as $row)
+                {
+                    if ($row->booking_user_id == 1)
+                    {
+                        foreach ($query2->result() as $row2)
+                        {
+                            if ($row->booking_venue_id == $row2->venue_id)
+                            {
+                                echo "<tr>";
+                                echo"<td><font color='black'>$row2->venue_id</font></td>";
+                                echo"<td><font color='black'>$row2->venue_name</font></td>";
+                                echo"<td><font color='black'>$row2->venue_capacity</font></td>";
+                                echo"<td><font color='black'>$row2->venue_location</font></td>";
+                                echo"<td><font color='red'><b>$row2->venue_expiry</b></font></td>";    
+                            }
+                        }
+                    }
+                }   
+                
+                echo "</tr>";
+            ?>
+        </tbody>
+
+    </table>    
 
 </div>
