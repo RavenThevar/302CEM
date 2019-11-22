e16d5315669718bbf28e068356cbd2b0fd01d791
RavenThevar <53144457+RavenThevar@users.noreply.github.com>
2019-11-22 22:19:01 +0800

    Optimisation done to 302CEM\VRS\application\views\home
    The homepage is further optimised to suit various new and existing features in the program.

--- VRS/application/views/home.php
+++ VRS/application/views/home.php
@@ -8,12 +8,15 @@
        
         <h1>Welcome to VRS - Venue Reservation System.</h1>
         </br>
-        <p>In this website: You can do the followings things:-</p>
-            <p>1. Add/View Venue</p>
-            <p>2. Make/View Reservation</p>
-            <p>3. Add/View Venue Slots</p>
-            <p>4. Login for Admin</p>
-            <p>5. Login for User</p>
+        <p>In this website: Depending on your role (Customer/Admin), you can do the followings things:-</p>
+            
+            <p>1. View / Book Venue (Customer Only)</p>
+            <p>2. View / Cancel Booked Venue (Customer Only)</p>
+            <p>3. View Notifications (Customer Only)</p>
+            <p>4. View / Alter Venue (Admin Only)</P>
+            <p>5. Add Venue (Admin Only)</p>
+            <p>6. Login for Customer & Admin</p>
+            <p>7. Register for Customer</p>
 
     </div>
 </div>
